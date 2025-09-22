7da3b4fe6df441de01cc69a31364f9dc3f08fdd5
ALANY <56726183+woanny@users.noreply.github.com>
2025-09-22 17:38:45 +0000

    feito durante a aula antes de atualizar o coiso bugado

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,24 +1,33 @@
 class Towel:
-    def __init__(self, color: str, size: str, wetness: int):
+    def __init__(self, color: str, size: str):
         
         self.color: str = color
         self.size: str = size # P M G
         self.wetness: int = 0 # max umidade: P -> 10 M -> 20 G -> 30
 
-    def getMaxWetness(self): #umidade máxima por tamanho
+    def dry (self, amount: int): #quantidade de umidade recebido
+
+        self.wetness += amount # deveria aumentar a umidade
+        
+        if self.wetness > self.getMaxWetness():
+            print('Toalha encharcada.')
+            self.wetness = self.getMaxWetness()
+
+    def wringOut (self): # torce a toalha
+        self.wetness = 0
+
+    def getMaxWetness(self): # umidade máxima por tamanho
         if self.size == 'P':
             return 10
         elif self.size == 'M':
             return 20
         elif self.size == 'G':
             return 30
+        return 0 # pra dizer quando nenhuma das opções for válida
 
-    def dry (self, amount: int): #quantidade de umidade recebido
-
-        self.wetness +- amount # deveria aumentar a umidade
-
-        if self.wetness > self.getMaxWetness():
-            self.wetness = self.getMaxWetness()
-
-    def wringOut (self):
-        self.wetness = 0
+def main():
+    toalha = Towel('', '') 
+    while True:
+        print('$', end='')
+        line: str = input()
+        args: list[str] = line.split('')
