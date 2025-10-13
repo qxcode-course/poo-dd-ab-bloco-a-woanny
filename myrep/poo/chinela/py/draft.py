8d4c4d94bf5c3f5785dd6ff1ec822f9b6ae01d66
ALANY <56726183+woanny@users.noreply.github.com>
2025-10-13 00:16:33 +0000

    Chinela finalizada

--- myrep/poo/chinela/py/draft.py
+++ myrep/poo/chinela/py/draft.py
@@ -2,24 +2,22 @@ class Chinela:
     def __init__(self):
         self.__tam: int = 0
 
-    def getTamanho(self, value: int):
+    def getTamanho(self):
         return self.__tam
 
     def setTamanho(self, value: int):
-        if value < 20 or value > 50 and valor % 2 != 0:
-            print('Erro: não exite gente com pé desse tamanho.')
-            return self
+        if value % 2 != 0 or value < 20 or value > 50:
+            print('Erro: não existe gente com pé desse tamanho.')
+            return
         self.__tam = value
-        return self
     
 def main():
-
     chinela = Chinela()
 
-    while chinela.getTamanho == 0:
+    while chinela.getTamanho() == 0:
         print('Digite o tamanho da sua chinela')
-        resposta = input()
+        resposta = int(input())
+        chinela.setTamanho(resposta)
 
-
-print('Parabéns, você comprou uma chinela tamanho ', chinela.getTamanho())
+    print('Parabéns, você comprou uma chinela tamanho', chinela.getTamanho())
 main()
